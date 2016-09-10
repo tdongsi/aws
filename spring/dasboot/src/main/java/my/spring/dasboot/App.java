9211160b06c139237e50326dbcf914084c6fedcb
tdongsi <dongsi.tuecuong@gmail.com>
2016-09-09 23:50:48 -0700

    Spring: Files generated by Eclipse's "Maven Project".

---
+++ spring/dasboot/src/main/java/my/spring/dasboot/App.java
@@ -0,0 +1,13 @@
+package my.spring.dasboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
