b6ad3164a2986536e1964830cbf8b7f29d6e2989
tdongsi <dongsi.tuecuong@gmail.com>
2016-09-12 00:26:32 -0700

    Spring: Clean up.

--- spring/dasboot/src/main/java/my/spring/dasboot/repository/ShipwreckRepository.java
+++
@@ -1,9 +0,0 @@
-package my.spring.dasboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import my.spring.dasboot.model.Shipwreck;
-
-public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
-
-}
